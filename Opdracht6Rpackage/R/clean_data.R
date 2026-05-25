d1f4cc02166857526305331e2003d4a5922ab7dc
DeHeerDani <Dani.pfeijffer@student.hu.nl>
2026-05-26 00:03:30 +0200

    deleted naar package repo

--- Opdracht6Rpackage/R/clean_data.R
+++
@@ -1,9 +0,0 @@
-#' Remove missing values from a dataset
-#'
-#' @param df data.frame
-#' @return cleaned data.frame
-#' @export
-clean_data <- function(df) {
-  df <- na.omit(df)
-  return(df)
-}
--- Opdracht6Rpackage/R/na_count.R
+++
@@ -1,8 +0,0 @@
-#' Count NA values
-#'
-#' @param x vector
-#' @return number of NA values
-#' @export
-na_count <- function(x) {
-  sum(is.na(x))
-}
